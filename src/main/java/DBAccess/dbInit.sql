9e24882a457c01cc27254f71ad060b673b9b6ba8
Marc <marc.g.ekstrom@gmail.com>
2020-03-11 15:14:26 +0100

    SQL setup added

--- src/main/java/DBAccess/dbInit.sql
+++ src/main/java/DBAccess/dbInit.sql
@@ -2,10 +2,8 @@
 
 -- noinspection SqlNoDataSourceInspectionForFile
 
-CREATE DATABASE  IF NOT EXISTS `useradmin`;
-
-
-USE `useradmin`;
+CREATE DATABASE  IF NOT EXISTS `Cupcake`;
+USE `Cupcake`;
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -13,14 +11,35 @@ CREATE TABLE `users` (
   `email` varchar(90) NOT NULL,
   `password` varchar(45) NOT NULL,
   `role` varchar(20) NOT NULL DEFAULT 'customer',
+  money float NOT null,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-LOCK TABLES `users` WRITE;
-INSERT INTO `users` VALUES 
-(1,'jens@somewhere.com','jensen','customer'),
-(2,'ken@somewhere.com','kensen','customer'),
-(3,'robin@somewhere.com','batman','employee');
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `topings`;
+CREATE TABLE `topings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(90) NOT NULL,
+  price float NOT null,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `bottom`;
+CREATE TABLE `bottom` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(90) NOT NULL,
+  price float NOT null,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `users` VALUES
+(1,'jens@somewhere.com','jensen','customer', 500),
+(2,'ken@somewhere.com','kensen','customer' , 500),
+(3,'robin@somewhere.com','batman','employee', 500);
+
+
+
 
