cbfcc893a80c2748889fbf1818fa616963bd09ed
Marc <marc.g.ekstrom@gmail.com>
2020-03-13 12:54:44 +0100

    SQL

--- Cupcake/src/main/java/DBAccess/dbInit.sql
+++ Cupcake/src/main/java/DBAccess/dbInit.sql
@@ -34,11 +34,36 @@ CREATE TABLE `bottom` (
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `pris` float NOT NULL,
+  betalt boolean NOT null,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  foreign key (user_id) references  users(id)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `Cupcakes`;
+CREATE TABLE `Cupcakes` (
+  `order_id` int(11) NOT NULL,
+  `top_id` int(11) NOT NULL,
+  `bottom_id` INT(11) NOT NULL,
+  foreign key (order_id) references  `order`(id),
+  foreign key (top_id) references  topings(id),
+  foreign key (bottom_id) references  bottom(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
 
 INSERT INTO `users` VALUES
 (1,'jens@somewhere.com','jensen','customer', 500),
 (2,'ken@somewhere.com','kensen','customer' , 500),
 (3,'robin@somewhere.com','batman','employee', 500);
+
+
 
 
 
